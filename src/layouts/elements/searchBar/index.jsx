1cd0b21066e86f5a4e7797ad0d0c26e3963a8221
Mopano <p.r.7.norouzi@gmail.com>
2024-09-11 00:02:01 -0700

    feature: added Task Header

---
+++ src/layouts/elements/searchBar/index.jsx
@@ -0,0 +1,64 @@
+import { alpha, InputBase, styled } from '@mui/material';
+import { CiSearch } from 'react-icons/ci';
+
+const Search = styled('div')(({ theme }) => ({
+	position: 'relative',
+	borderRadius: theme.shape.borderRadius,
+	border: '1px solid',
+	display: 'flex',
+	borderColor: theme.palette.background.lightGray,
+	backgroundColor: alpha(theme.palette.common.white, 0.15),
+	'&:hover': {
+		backgroundColor: alpha(theme.palette.common.white, 0.25),
+	},
+	marginLeft: 0,
+	width: '33%',
+	[theme.breakpoints.up('sm')]: {
+		marginLeft: theme.spacing(1),
+		width: '33%',
+	},
+}));
+
+const SearchIconWrapper = styled('div')(() => ({
+	paddingRight: '14px',
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	right: 0,
+	fontSize: '20px',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+	fontSize: '12px',
+	fontWeight: 400,
+	color: 'inherit',
+	width: '100%',
+	'& .MuiInputBase-input': {
+		padding: '14px, 28px, 14px, 28px',
+		// vertical padding + font size from searchIcon
+		paddingLeft: '1em',
+		transition: theme.transitions.create('width'),
+		[theme.breakpoints.up('sm')]: {
+			width: '12ch',
+			'&:focus': {
+				width: '20ch',
+			},
+		},
+	},
+}));
+
+const SearchBar = () => {
+	return (
+		<Search>
+			<SearchIconWrapper>
+				<CiSearch />
+			</SearchIconWrapper>
+			<StyledInputBase placeholder='Search Task' inputProps={{ 'aria-label': 'search' }} />
+		</Search>
+	);
+};
+
+export default SearchBar;
---
+++ src/pages/task/components/info/index.jsx
@@ -0,0 +1,119 @@
+import { useTheme } from '@emotion/react';
+import {
+	Avatar,
+	Box,
+	List,
+	ListItem,
+	ListItemAvatar,
+	ListItemText,
+	Stack,
+	SvgIcon,
+	Typography,
+} from '@mui/material';
+import { CgSortAz } from 'react-icons/cg';
+import { IoNotificationsOutline } from 'react-icons/io5';
+import { TbCategory2 } from 'react-icons/tb';
+import SearchBar from '../../../../layouts/elements/searchBar';
+
+const Info = () => {
+	const theme = useTheme();
+	return (
+		<Box>
+			<Box sx={{ display: 'flex', justifyContent: 'space-between', p: 1 }}>
+				<Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+					<Typography sx={{ fontWeight: 600, fontSize: 24 }} color={theme.palette.text.darkBlue}>
+						Explore Task
+					</Typography>
+				</Box>
+				<Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+					<Box className='icon'>
+						<SvgIcon
+							sx={{
+								height: '24px',
+								width: '24px',
+								color: theme.palette.text.lightWhite,
+							}}
+						>
+							<IoNotificationsOutline />
+						</SvgIcon>
+					</Box>
+					<Stack direction='row' spacing={2} className='icon'>
+						<Avatar
+							alt='Remy Sharp'
+							src='/images/users/user.jpg'
+							sx={{ height: '52px', width: '52px' }}
+						/>
+					</Stack>
+				</Box>
+			</Box>
+			<Box
+				sx={{
+					display: 'flex',
+					justifyContent: 'space-between',
+					width: '100%',
+
+					alignItems: 'center',
+				}}
+			>
+				<SearchBar />
+				<List sx={{ display: 'flex', gap: 2 }}>
+					<ListItem
+						sx={{
+							width: 'max-content',
+							minHeight: 52,
+							display: 'flex',
+							gap: '12px',
+							cursor: 'pointer',
+							':hover': { background: theme.palette.background.lightGray },
+							borderRadius: '10px',
+						}}
+					>
+						<ListItemAvatar
+							sx={{
+								minWidth: '0',
+								fontSize: '24px',
+								display: 'flex',
+								color: theme.palette.text.lightWhite,
+							}}
+						>
+							<TbCategory2 />
+						</ListItemAvatar>
+						<ListItemText
+							secondary='Category'
+							sx={{ fontSize: '12px', fontWeight: 600, textAlign: 'center' }}
+						/>
+					</ListItem>
+					<ListItem
+						sx={{
+							width: 'max-content',
+							minHeight: 52,
+							display: 'flex',
+							gap: '12px',
+							cursor: 'pointer',
+							':hover': { background: theme.palette.background.lightGray },
+							borderRadius: '10px',
+							color: theme.palette.text.darkBlue,
+						}}
+					>
+						<ListItemAvatar
+							sx={{
+								minWidth: '0',
+								fontSize: '24px',
+								display: 'flex',
+								color: theme.palette.text.lightWhite,
+							}}
+						>
+							<CgSortAz />
+						</ListItemAvatar>
+						<ListItemText
+							secondary='Sort By : Deadline'
+							sx={{ fontSize: '12px', fontWeight: 600 }}
+						/>
+					</ListItem>
+				</List>
+			</Box>
+		</Box>
+	);
+};
+
+export default Info;
